e53ee4184fd3d229b9ebb36e2e82da9d3d7ba8de
fmdallafontana <fmdallafontana@outlook.com>
2014-12-20 15:54:07 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are cases in programming that When you want to calculate the inverse of a matrix you don't do it just once. In these cases may be useful to cache the result in case you'll need it after, and so reduce  time-consuming computations.
+
 
 ## Write a short comment describing this function
 
